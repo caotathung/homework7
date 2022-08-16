6cf837132e68c42801ce2ac6608c2b63f2d6ea23
caotathung <110296614+caotathung@users.noreply.github.com>
2022-08-16 17:33:47 +0700

    first commit

---
+++ lesson7/index.js
@@ -0,0 +1,21 @@
+// alert("chao cac ban")
+// confirm("Bam vao nut oke de dong y va tiep tuc")
+// console.log('chao cac ban')
+// let age = 10;
+// console.log('age',age);
+//prompt
+// prompt('nhap ho ten cua ban','Hung')
+let greenting='Hello. My name is Jhon'
+let ab = greenting.charAt(8)
+console.log(ab);
+
+let str1 =('The morning is upon us.');
+let c = str1.slice(11)
+console.log(c);
+
+
+
+let str2 = 'Hello';
+let str3 = 'World';
+let str = str2.concat(' ', str3);
+console.log(str)
